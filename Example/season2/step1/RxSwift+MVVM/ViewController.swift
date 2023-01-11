2dff2633f22568ab3faf431a20779407d70e542d
usa4060 <cocoa9805@naver.com>
2023-01-12 03:44:03 +0900

    RxSwift 4시간에 끝내기 - 1
    RxSwift 개념잡기 (등장배경 + 기본구조)

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -12,17 +12,29 @@ import UIKit
 
 let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
 
+//class Observable<T> {
+//    private let task : (@escaping (T) -> Void)-> Void
+//
+//    init(task: @escaping (@escaping (T) -> Void) -> Void) {
+//        self.task = task
+//    }
+//
+//    func subscribe(_ f : @escaping (T) -> Void){
+//        task(f)
+//    }
+//}
+
 class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
     }
-
+    
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
         guard let v = v else { return }
         UIView.animate(withDuration: 0.3, animations: { [weak v] in
@@ -31,20 +43,75 @@ class ViewController: UIViewController {
             self?.view.layoutIfNeeded()
         })
     }
-
+    
+    
+    // 1. 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
+    func downloadJson(_ url : String )-> Observable<String?> {
+        
+       return Observable.create(){ emit in
+           let url = URL(string: MEMBER_LIST_URL)!
+           
+           let task = URLSession.shared.dataTask(with: url){(data, _, err) in
+               guard err == nil else{
+                   emit.onError(err!)
+                   return
+               }
+               
+               if let data = data, let json = String(data: data, encoding: .utf8){
+                   emit.onNext(json)
+               }
+               
+               emit.onCompleted()
+           }
+           
+           task.resume()
+            
+           return Disposables.create(){
+               task.cancel()
+           }
+        }
+        
+//        return Observable.create{f in
+//            DispatchQueue.global().async {
+//                let url = URL(string: MEMBER_LIST_URL)!
+//                let data = try! Data(contentsOf: url)
+//                let json = String(data: data, encoding: .utf8)
+//                DispatchQueue.main.async {
+//                    f.onNext(json)
+//                }
+//            }
+//            return Disposables.create()
+//        }
+    }
+    
     // MARK: SYNC
-
+    
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
-
+    
+    
+    
+    
     @IBAction func onLoad() {
         editView.text = ""
-        setVisibleWithAnimation(activityIndicator, true)
-
-        let url = URL(string: MEMBER_LIST_URL)!
-        let data = try! Data(contentsOf: url)
-        let json = String(data: data, encoding: .utf8)
-        self.editView.text = json
+        self.setVisibleWithAnimation(self.activityIndicator, true)
         
-        self.setVisibleWithAnimation(self.activityIndicator, false)
+        let observable = downloadJson(MEMBER_LIST_URL)
+        
+        // 2. Observable로 오는 데이터를 받아서 처리하는 방법
+    
+       let disposable = observable.subscribe(){ [weak self] event in
+            switch event{
+                case .next(let json):
+                    break
+                case .error(let err):
+                    break
+                case .completed:
+                    break
+            }
+        }
+        disposable.dispose()
     }
 }
+
+
+
