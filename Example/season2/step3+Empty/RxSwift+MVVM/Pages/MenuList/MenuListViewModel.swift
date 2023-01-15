dcca8904b155216db2479fe017fe8edd523b9f32
usa4060 <cocoa9805@naver.com>
2023-01-16 02:00:02 +0900

    RxSwift 4시간에 끝내기 - 5
    Subject는 무엇일까??

--- Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
+++ Example/season2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -10,14 +10,40 @@ import Foundation
 import RxSwift
 
 
+//
+//  MenuListViewModel.swift
+//  RxSwift+MVVM
+//
+//  Created by admin on 2023/01/29.
+//  Copyright © 2023 iamchiwon. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+
 class MenuListViewModel{
-    var menus : [Menu] = [
-        Menu(name: "튀김1", price: 100, count: 0),
-        Menu(name: "튀김1", price: 100, count: 0),
-        Menu(name: "튀김1", price: 100, count: 0),
-        Menu(name: "튀김1", price: 100, count: 0)
-    ]
     
-    var itemsCount : Int = 5
-    var totalPrice : Observable<Int> = Observable.just(10_000)
+    var menusObservable = PublishSubject<[Menu]>()
+    
+    lazy var itemsCount = menusObservable
+        .map{
+            $0.map{$0.count}.reduce(0,+)
+        }
+    lazy var totalPrice = menusObservable
+        .map{
+            $0.map{$0.price * $0.count}.reduce(0,+)
+        }
+    
+    init(){
+        var menus : [Menu] = [
+            Menu(name: "튀김1", price: 100, count: 0),
+            Menu(name: "튀김1", price: 100, count: 0),
+            Menu(name: "튀김1", price: 100, count: 0),
+            Menu(name: "튀김1", price: 100, count: 0)
+        ]
+        menusObservable.onNext(menus)
+    }
+    
 }
+
